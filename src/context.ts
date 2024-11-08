e4b5bc2fa595a317984fb41ce20454b554eae081
fly2z <69116434+metebykl@users.noreply.github.com>
2024-11-08 11:18:50 +0300

    refactor: unify context and request classes [wip]

--- src/context.ts
+++ src/context.ts
@@ -1,19 +1,56 @@
 import { SpectraRequest } from "./request";
+import type { ParamKeys, ParamsToRecord } from "./types";
 import type { OutgoingHttpHeaders } from "./utils/headers";
-import type { HTTPMethod } from "./types";
 
-export interface Context<Path extends string = "/"> {
-  method: HTTPMethod;
-  path: Path;
-  req: SpectraRequest<Path>;
+export type Data = string | ArrayBuffer | ReadableStream;
 
-  setHeader(
-    name: OutgoingHttpHeaders | (string & {}),
-    value: string | number
-  ): void;
+export class Context<P extends string = "/"> {
+  path: P;
+  method: string;
+  req: SpectraRequest<P>;
 
-  json(data: unknown, status?: number): void;
-  text(data: string, status?: number): void;
-  html(data: string, status?: number): void;
-  redirect(location: string, status?: number): void;
+  private rawRequest: Request;
+  private headers: Headers;
+
+  constructor(req: Request, params: ParamsToRecord<ParamKeys<P>>) {
+    this.path = req.url as P;
+    this.method = req.method;
+
+    this.rawRequest = req;
+    this.req = new SpectraRequest(req, req.url, params);
+    this.headers = new Headers();
+  }
+
+  setHeader(name: OutgoingHttpHeaders | (string & {}), value: string): void {
+    this.headers.set(name, value);
+  }
+
+  private newResponse(data: Data | null, arg?: ResponseInit): Response {
+    return new Response(data, {
+      status: arg?.status,
+      headers: this.headers,
+    });
+  }
+
+  json(data: unknown, status = 200): Response {
+    const body = JSON.stringify(data);
+
+    this.headers.set("Content-Type", "application/json");
+    return this.newResponse(body, { status });
+  }
+
+  text(data: string, status = 200): Response {
+    this.headers.set("Content-Type", "text/plain");
+    return this.newResponse(data, { status });
+  }
+
+  html(data: string, status = 200): Response {
+    this.headers.set("Content-Type", "text/html");
+    return this.newResponse(data, { status });
+  }
+
+  redirect(location: string, status = 302): Response {
+    this.headers.set("Location", location);
+    return this.newResponse(null, { status });
+  }
 }
--- src/request.ts
+++ src/request.ts
@@ -1,20 +1,53 @@
+import { getQueryParam } from "./utils/url";
 import type { ParamKeys, ParamsToRecord } from "./types";
 import type { IncomingHttpHeaders } from "./utils/headers";
 
-export interface SpectraRequest<P extends string = "/"> {
-  path: P;
+export class SpectraRequest<P extends string = "/"> {
+  raw: Request;
+  path: string;
 
-  param<K extends ParamKeys<P>>(key: K): string;
-  params(): ParamsToRecord<ParamKeys<P>>;
+  private _params: ParamsToRecord<ParamKeys<P>>;
+
+  constructor(
+    request: Request,
+    path: string = "/",
+    params: ParamsToRecord<ParamKeys<P>>
+  ) {
+    this.raw = request;
+    this.path = path;
+    this._params = params;
+  }
+
+  param<K extends ParamKeys<P>>(key: K): string {
+    return this._params[key];
+  }
+
+  params(): ParamsToRecord<ParamKeys<P>> {
+    return this._params;
+  }
 
   query(key: string): string | undefined;
   query(): Record<string, string>;
+  query(key?: string) {
+    return getQueryParam(this.path, key);
+  }
 
   queries(key: string): string[] | undefined;
   queries(): Record<string, string[]>;
+  queries(key?: string) {
+    return getQueryParam(this.path, key, true);
+  }
 
-  header(name: IncomingHttpHeaders | (string & {})): string | undefined;
+  header(name: IncomingHttpHeaders | (string & {})): string | undefined {
+    const key = name.toLowerCase();
+    return this.raw.headers.get(key) ?? undefined;
+  }
 
-  json(): Promise<any>;
-  text(): Promise<string>;
+  async json(): Promise<any> {
+    return await this.raw.json();
+  }
+
+  async text(): Promise<string> {
+    return await this.raw.text();
+  }
 }
