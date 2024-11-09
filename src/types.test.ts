c4c5c2a04374ad7ad6973a6f151615370a520ab9
fly2z <69116434+metebykl@users.noreply.github.com>
2024-11-09 11:29:02 +0300

    test: add vitest and tests for path parameter types

---
+++ src/types.test.ts
@@ -0,0 +1,34 @@
+import { describe, test } from "vitest";
+import { Spectra } from "./spectra";
+import type { Equal, Expect } from "./utils/types";
+
+describe("With path parameters", () => {
+  const app = new Spectra();
+
+  test("Should handle single path parameters", () => {
+    app.get("/user/:id", (c) => {
+      const params = c.req.params();
+      type Expected = {
+        id: string;
+      };
+
+      type Verify = Expect<Equal<typeof params, Expected>>;
+
+      return c.text("OK");
+    });
+  });
+
+  test("Should handle multiple path parameters", () => {
+    app.get("/user/:userId/post/:postId", (c) => {
+      const params = c.req.params();
+      type Expected = {
+        userId: string;
+        postId: string;
+      };
+
+      type Verify = Expect<Equal<typeof params, Expected>>;
+
+      return c.text("OK");
+    });
+  });
+});
