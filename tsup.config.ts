8c8c87301d4cafdf16328c5065ef7454bc18adf1
fly2z <69116434+metebykl@users.noreply.github.com>
2024-11-14 15:08:51 +0300

    refactor(build): update package.json and tsup config for improved exports

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: [
-    "src/index.ts",
-    "src/middleware/index.ts",
-    "src/adapter/node/index.ts",
-  ],
-  format: ["cjs", "esm"],
+  entry: ["src/**/*.ts", "!src/**/*.test.ts"],
+  format: ["esm", "cjs"],
   dts: true,
+  splitting: false,
+  sourcemap: false,
+  bundle: false,
   clean: true,
 });
