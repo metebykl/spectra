5bbe05513d9b1d27f240184c0a63a46d7e235f20
fly2z <69116434+metebykl@users.noreply.github.com>
2024-11-14 21:07:46 +0300

    test(cookie): add tests for setCookie()

--- src/cookie.test.ts
+++ src/cookie.test.ts
@@ -1,6 +1,6 @@
 import { describe, test, expect } from "vitest";
 import { Spectra } from "./spectra";
-import { getCookie } from "./cookie";
+import { getCookie, setCookie } from "./cookie";
 
 describe("Parse cookie", () => {
   const app = new Spectra();
@@ -30,5 +30,115 @@ describe("Parse cookie", () => {
     const body = await res.json();
 
     expect(body).toEqual({ foo: "bar", hello: "world" });
+  });
+});
+
+describe("Set cookie", () => {
+  const app = new Spectra();
+  const request = new Request("http://localhost/");
+
+  test("Set single cookie", async () => {
+    app.get("/", (c) => {
+      setCookie(c, "foo", "bar");
+      return c.text("OK");
+    });
+
+    const res = await app.fetch(request);
+    expect(res.headers.get("Set-Cookie")).toBe("foo=bar");
+  });
+
+  test("Set cookie Path", async () => {
+    app.get("/", (c) => {
+      setCookie(c, "foo", "bar", {
+        path: "/",
+      });
+
+      return c.text("OK");
+    });
+
+    const res = await app.fetch(request);
+    expect(res.headers.get("Set-Cookie")).toBe(`foo=bar; Path=/`);
+  });
+
+  test("Set cookie SameSite", async () => {
+    app.get("/", (c) => {
+      setCookie(c, "foo", "bar", {
+        sameSite: "Strict",
+      });
+
+      return c.text("OK");
+    });
+
+    const res = await app.fetch(request);
+    expect(res.headers.get("Set-Cookie")).toBe(`foo=bar; SameSite=Strict`);
+  });
+
+  test("Set cookie Expires", async () => {
+    const expire = new Date(Date.now() + 1000 * 60);
+
+    app.get("/", (c) => {
+      setCookie(c, "foo", "bar", {
+        expires: expire,
+      });
+
+      return c.text("OK");
+    });
+
+    const res = await app.fetch(request);
+    expect(res.headers.get("Set-Cookie")).toBe(
+      `foo=bar; Expires=${expire.toUTCString()}`
+    );
+  });
+
+  test("Set cookie MaxAge", async () => {
+    app.get("/", (c) => {
+      setCookie(c, "foo", "bar", {
+        maxAge: 60 * 60,
+      });
+
+      return c.text("OK");
+    });
+
+    const res = await app.fetch(request);
+    expect(res.headers.get("Set-Cookie")).toBe(`foo=bar; Max-Age=3600`);
+  });
+
+  test("Set cookie HttpOnly", async () => {
+    app.get("/", (c) => {
+      setCookie(c, "foo", "bar", {
+        httpOnly: true,
+      });
+
+      return c.text("OK");
+    });
+
+    const res = await app.fetch(request);
+    expect(res.headers.get("Set-Cookie")).toBe(`foo=bar; HttpOnly`);
+  });
+
+  test("Set multiple cookies", async () => {
+    app.get("/", (c) => {
+      setCookie(c, "foo", "bar");
+      setCookie(c, "hello", "world");
+
+      return c.text("OK");
+    });
+
+    const res = await app.fetch(request);
+    expect(res.headers.get("Set-Cookie")).toBe("foo=bar, hello=world");
+  });
+
+  test("Set multiple cookies with Path", async () => {
+    app.get("/", (c) => {
+      setCookie(c, "foo", "bar", { path: "/" });
+      setCookie(c, "hello", "world", { path: "/test" });
+
+      return c.text("OK");
+    });
+
+    const res = await app.fetch(request);
+    expect(res.headers.get("Set-Cookie")).toBe(
+      "foo=bar; Path=/, hello=world; Path=/test"
+    );
   });
 });
