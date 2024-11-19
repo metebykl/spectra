6f8b5def569847a5b2712914039c26bca5e0e86e
fly2z <69116434+metebykl@users.noreply.github.com>
2024-11-19 21:49:34 +0300

    feat(middleware): enhance logger middleware with more details and add colorization option

--- src/middleware/logger/index.test.ts
+++ src/middleware/logger/index.test.ts
@@ -16,11 +16,76 @@ describe("Logger Middleware", () => {
     app.use(logger({ fn }));
 
     app.get("/", (c) => c.text("OK"));
+    app.get("/redirect", (c) => c.redirect("/other"));
+    app.get("/empty", (c) => c.notFound());
+    app.get("/error", (c) => c.text("Internal Server Error", 500));
   });
 
   test("Log status 200", async () => {
     const res = await app.fetch(new Request("http://localhost/"));
     expect(res.status).toBe(200);
-    expect(log).toBe("GET /");
+    expect(log.startsWith("GET / \x1b[32m200\x1b[0m")).toBe(true);
+  });
+
+  test("Log status 302", async () => {
+    const res = await app.fetch(new Request("http://localhost/redirect"));
+    expect(res.status).toBe(302);
+    expect(log.startsWith("GET /redirect \x1b[36m302\x1b[0m")).toBe(true);
+  });
+
+  test("Log status 404", async () => {
+    const res = await app.fetch(new Request("http://localhost/empty"));
+    expect(res.status).toBe(404);
+    expect(log.startsWith("GET /empty \x1b[33m404\x1b[0m")).toBe(true);
+  });
+
+  test("Log status 500", async () => {
+    const res = await app.fetch(new Request("http://localhost/error"));
+    expect(res.status).toBe(500);
+    expect(log.startsWith("GET /error \x1b[31m500\x1b[0m")).toBe(true);
+  });
+});
+
+describe("Logger Middleware NO_COLOR", () => {
+  let app: Spectra;
+  let log: string;
+
+  beforeEach(() => {
+    app = new Spectra();
+
+    const fn = (str: string) => {
+      log = str;
+    };
+
+    app.use(logger({ fn, colorize: false }));
+
+    app.get("/", (c) => c.text("OK"));
+    app.get("/redirect", (c) => c.redirect("/other"));
+    app.get("/empty", (c) => c.notFound());
+    app.get("/error", (c) => c.text("Internal Server Error", 500));
+  });
+
+  test("Log status 200", async () => {
+    const res = await app.fetch(new Request("http://localhost/"));
+    expect(res.status).toBe(200);
+    expect(log.startsWith("GET / 200")).toBe(true);
+  });
+
+  test("Log status 302", async () => {
+    const res = await app.fetch(new Request("http://localhost/redirect"));
+    expect(res.status).toBe(302);
+    expect(log.startsWith("GET /redirect 302")).toBe(true);
+  });
+
+  test("Log status 404", async () => {
+    const res = await app.fetch(new Request("http://localhost/empty"));
+    expect(res.status).toBe(404);
+    expect(log.startsWith("GET /empty 404")).toBe(true);
+  });
+
+  test("Log status 500", async () => {
+    const res = await app.fetch(new Request("http://localhost/error"));
+    expect(res.status).toBe(500);
+    expect(log.startsWith("GET /error 500")).toBe(true);
   });
 });
--- src/middleware/logger/index.ts
+++ src/middleware/logger/index.ts
@@ -4,10 +4,40 @@ type PrintFn = (str: string, ...rest: string[]) => void;
 
 type LoggerOptions = {
   fn?: PrintFn;
+  colorize?: boolean;
 };
 
-const log = (fn: PrintFn, method: string, path: string) => {
-  const str = `${method} ${path}`;
+const getElapsed = (start: number, end: number): string => {
+  const elapsed = end - start;
+  return elapsed < 1000 ? `${elapsed}ms` : `${(elapsed / 1000).toFixed(2)}s`;
+};
+
+const colorizeStatus = (status: number): string => {
+  switch ((status / 100) | 0) {
+    case 5: // error = red
+      return `\x1b[31m${status}\x1b[0m`;
+    case 4: // warning = yellow
+      return `\x1b[33m${status}\x1b[0m`;
+    case 3: // redirect = cyan
+      return `\x1b[36m${status}\x1b[0m`;
+    case 2: // success = green
+      return `\x1b[32m${status}\x1b[0m`;
+  }
+
+  return `${status}`;
+};
+
+const log = (
+  fn: PrintFn,
+  method: string,
+  path: string,
+  status: number,
+  elapsed: string,
+  colorize: boolean = true
+) => {
+  const str = colorize
+    ? `${method} ${path} ${colorizeStatus(status)} ${elapsed}`
+    : `${method} ${path} ${status} ${elapsed}`;
   fn(str);
 };
 
@@ -15,10 +45,21 @@ export const logger = (options?: LoggerOptions): MiddlewareHandler => {
   return async function (c, next) {
     const method = c.method;
     const path = c.path;
-
     const fn = options?.fn ?? console.log;
-    log(fn, method, path);
+
+    const start = Date.now();
 
     await next();
+
+    const elapsed = getElapsed(start, Date.now());
+
+    log(
+      fn,
+      method,
+      path,
+      c.res.status,
+      elapsed.toString(),
+      options?.colorize ?? true
+    );
   };
 };
