fcd6d449859b5559fe9b3517710ae6fc95959275
fly2z <69116434+metebykl@users.noreply.github.com>
2024-11-25 17:37:29 +0300

    test(spectra): add tests for base path and route chaining

--- src/spectra.test.ts
+++ src/spectra.test.ts
@@ -2,6 +2,44 @@ import { describe, test, expect } from "vitest";
 import { Spectra } from "./spectra";
 import { poweredBy } from "./middleware";
 
+describe("Routing", () => {
+  test("Base path", async () => {
+    const app = new Spectra("/api");
+
+    app.get("/", (c) => c.json({ message: "Test" }));
+
+    let res = await app.fetch(new Request("http://localhost/"));
+    expect(res.status).toBe(404);
+
+    res = await app.fetch(new Request("http://localhost/api"));
+    expect(res.status).toBe(200);
+    expect(await res.json()).toEqual({ message: "Test" });
+  });
+
+  test("Chaining", async () => {
+    const app = new Spectra();
+
+    let app2 = app.use("/", async (_, next) => {
+      await next();
+    });
+    expect(app2).not.toBeUndefined();
+
+    app2 = app.get("/", (c) => c.text("Spectra"));
+    expect(app2).not.toBeUndefined();
+
+    app2 = app.notFound((c) => c.text("Not Found", 404));
+    expect(app2).not.toBeUndefined();
+
+    let res = await app.fetch(new Request("http://localhost/"));
+    expect(res.status).toBe(200);
+    expect(await res.text()).toBe("Spectra");
+
+    res = await app2.fetch(new Request("http://localhost/"));
+    expect(res.status).toBe(200);
+    expect(await res.text()).toBe("Spectra");
+  });
+});
+
 describe("Not Found", () => {
   const app = new Spectra();
 
