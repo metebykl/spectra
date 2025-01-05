b7d0d656d83fe271820ac40fe03152857f555b8f
fly2z <69116434+metebykl@users.noreply.github.com>
2025-01-05 20:33:57 +0300

    test(trpc): add coverage for synchronous and asynchronous context creation

--- packages/trpc/test/index.test.ts
+++ packages/trpc/test/index.test.ts
@@ -29,3 +29,87 @@ describe("tRPC Adapter", () => {
     expect(await res.json()).toEqual([{ result: { data: "Hi Spectra!" } }]);
   });
 });
+
+describe("tRPC Adapter Synchronous Context", () => {
+  type SpectraContext = {
+    batch: string;
+  };
+
+  const t = initTRPC.context<SpectraContext>().create();
+
+  const appRouter = t.router({
+    greet: t.procedure
+      .input(String)
+      .query(({ ctx, input }) => `Hello ${input}, batch is ${ctx.batch}`),
+  });
+
+  const app = new Spectra();
+  app.use(
+    "/trpc/*",
+    trpc({
+      endpoint: "/trpc",
+      router: appRouter,
+      createContext: (c) => ({
+        batch: c.req.query("batch"),
+      }),
+    })
+  );
+
+  test("Should return response 200", async () => {
+    const searchParams = new URLSearchParams({
+      input: JSON.stringify({ "0": "Spectra" }),
+      batch: "1",
+    });
+    const req = new Request(
+      `http://localhost/trpc/greet?${searchParams.toString()}`
+    );
+    const res = await app.fetch(req);
+
+    expect(res.status).toBe(200);
+    expect(await res.json()).toEqual([
+      { result: { data: "Hello Spectra, batch is 1" } },
+    ]);
+  });
+});
+
+describe("tRPC Adapter Asynchronous Context", () => {
+  type SpectraContext = {
+    batch: string;
+  };
+
+  const t = initTRPC.context<SpectraContext>().create();
+
+  const appRouter = t.router({
+    greet: t.procedure
+      .input(String)
+      .query(({ ctx, input }) => `Hello ${input}, batch is ${ctx.batch}`),
+  });
+
+  const app = new Spectra();
+  app.use(
+    "/trpc/*",
+    trpc({
+      endpoint: "/trpc",
+      router: appRouter,
+      createContext: async (c) => ({
+        batch: c.req.query("batch"),
+      }),
+    })
+  );
+
+  test("Should return response 200", async () => {
+    const searchParams = new URLSearchParams({
+      input: JSON.stringify({ "0": "Spectra" }),
+      batch: "1",
+    });
+    const req = new Request(
+      `http://localhost/trpc/greet?${searchParams.toString()}`
+    );
+    const res = await app.fetch(req);
+
+    expect(res.status).toBe(200);
+    expect(await res.json()).toEqual([
+      { result: { data: "Hello Spectra, batch is 1" } },
+    ]);
+  });
+});
