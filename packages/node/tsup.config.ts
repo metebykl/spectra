b9110ebb3478cac3f9c6920347fa5759bc415b5c
fly2z <69116434+metebykl@users.noreply.github.com>
2025-01-16 13:29:13 +0300

    chore: update tsup configuration for node and trpc packages

--- packages/node/tsup.config.ts
+++ packages/node/tsup.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/**/*.ts", "!src/**/*.test.ts"],
+  entry: ["src/index.ts"],
   format: ["esm", "cjs"],
   dts: true,
   splitting: false,
   sourcemap: false,
-  bundle: false,
+  bundle: true,
   clean: true,
 });
