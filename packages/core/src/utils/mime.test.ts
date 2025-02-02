9a5581bafd26982562fe3769cdec64cd08e5df11
Mete <69116434+metebykl@users.noreply.github.com>
2025-02-02 13:19:56 +0300

    feat(mime): add MIME type utils (#11)

---
+++ packages/core/src/utils/mime.test.ts
@@ -0,0 +1,24 @@
+import { describe, expect, test } from "vitest";
+import { getExtension, getMimeType } from "./mime";
+
+describe("Mime", () => {
+  test("getMimeType", () => {
+    expect(getMimeType("index.txt")).toBe("text/plain; charset=utf-8");
+    expect(getMimeType("index.html")).toBe("text/html; charset=utf-8");
+    expect(getMimeType("index.json")).toBe("application/json");
+    expect(getMimeType("image.png")).toBe("image/png");
+    expect(getMimeType("foo.bar.gif")).toBe("image/gif");
+    expect(getMimeType("index")).toBeUndefined();
+    expect(getMimeType("index.random")).toBeUndefined();
+  });
+
+  test("getExtension", () => {
+    expect(getExtension("application/octet-stream")).toBe("bin");
+    expect(getExtension("image/png")).toBe("png");
+    expect(getExtension("audio/mpeg")).toBe("mp3");
+    expect(getExtension("application/json")).toBe("json");
+    expect(getExtension("text/html")).toBe("htm");
+    expect(getExtension("application/zip")).toBe("zip");
+    expect(getExtension("foo/bar")).toBeUndefined();
+  });
+});
