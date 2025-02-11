e2c168e14ded22a53fbe1814ed395741e28cf7bd
fly2z <69116434+metebykl@users.noreply.github.com>
2025-02-11 22:33:28 +0300

    fix(build): implement custom esbuild plugin to fix import extensions

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -1,20 +1,53 @@
-import { defineConfig } from "tsup";
-import { esbuildPluginFilePathExtensions } from "esbuild-plugin-file-path-extensions";
+import { defineConfig, type Options } from "tsup";
+import type { Plugin, PluginBuild } from "esbuild";
+import fs from "node:fs";
+import path from "node:path";
 
-export default defineConfig({
+// https://github.com/evanw/esbuild/issues/622#issuecomment-769462611
+const fixImportExtensions = (extension: string = ".js"): Plugin => ({
+  name: "fix-import-extensions",
+  setup(build: PluginBuild) {
+    build.onResolve({ filter: /.*/ }, (args) => {
+      if (args.importer) {
+        let filePath = path.join(args.resolveDir, args.path) + ".ts";
+
+        let resolvedPath = "";
+        if (fs.existsSync(filePath)) {
+          resolvedPath = args.path + extension;
+        } else {
+          filePath = path.join(args.resolveDir, args.path, `index.ts`);
+          if (fs.existsSync(filePath)) {
+            if (args.path.endsWith("/")) {
+              resolvedPath = `${args.path}index${extension}`;
+            } else {
+              resolvedPath = `${args.path}/index${extension}`;
+            }
+          }
+        }
+        return { path: resolvedPath, external: true };
+      }
+    });
+  },
+});
+
+const commonOptions: Options = {
   entry: ["src/**/*.ts", "!src/**/*.test.ts"],
-  format: ["esm", "cjs"],
   dts: true,
   splitting: false,
   sourcemap: false,
   clean: true,
-  // https://github.com/egoist/tsup/issues/953
-  // https://github.com/favware/esbuild-plugin-file-path-extensions
-  bundle: true,
-  esbuildPlugins: [
-    esbuildPluginFilePathExtensions({
-      cjsExtension: "cjs",
-      esmExtension: "js",
-    }),
-  ],
-});
+  bundle: false,
+};
+
+export default defineConfig([
+  {
+    ...commonOptions,
+    format: "esm",
+    bundle: true,
+    esbuildPlugins: [fixImportExtensions()],
+  },
+  {
+    ...commonOptions,
+    format: "cjs",
+  },
+]);
