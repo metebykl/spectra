af94795e9fb824cc6a66442a6499776a63267569
Mete <69116434+metebykl@users.noreply.github.com>
2025-02-12 12:15:44 +0300

    fix(build): resolve missing import extensions (#18)
    * fix(build): add esbuild plugin to resolve missing import extensions
    
    * fix(build): implement custom esbuild plugin to fix import extensions
    
    * docs: update issue reference
    
    * chore: add changeset

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -1,11 +1,53 @@
-import { defineConfig } from "tsup";
+import { defineConfig, type Options } from "tsup";
+import type { Plugin, PluginBuild } from "esbuild";
+import fs from "node:fs";
+import path from "node:path";
 
-export default defineConfig({
+// https://github.com/egoist/tsup/issues/953
+const fixImportExtensions = (extension: string = ".js"): Plugin => ({
+  name: "fix-import-extensions",
+  setup(build: PluginBuild) {
+    build.onResolve({ filter: /.*/ }, (args) => {
+      if (args.importer) {
+        let filePath = path.join(args.resolveDir, args.path) + ".ts";
+
+        let resolvedPath = "";
+        if (fs.existsSync(filePath)) {
+          resolvedPath = args.path + extension;
+        } else {
+          filePath = path.join(args.resolveDir, args.path, `index.ts`);
+          if (fs.existsSync(filePath)) {
+            if (args.path.endsWith("/")) {
+              resolvedPath = `${args.path}index${extension}`;
+            } else {
+              resolvedPath = `${args.path}/index${extension}`;
+            }
+          }
+        }
+        return { path: resolvedPath, external: true };
+      }
+    });
+  },
+});
+
+const commonOptions: Options = {
   entry: ["src/**/*.ts", "!src/**/*.test.ts"],
-  format: ["esm", "cjs"],
   dts: true,
   splitting: false,
   sourcemap: false,
-  bundle: false,
   clean: true,
-});
+  bundle: false,
+};
+
+export default defineConfig([
+  {
+    ...commonOptions,
+    format: "esm",
+    bundle: true,
+    esbuildPlugins: [fixImportExtensions()],
+  },
+  {
+    ...commonOptions,
+    format: "cjs",
+  },
+]);
