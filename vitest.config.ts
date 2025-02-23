d0fab793ef0ce8ab270d358d18917ed7fa168933
Mete <69116434+metebykl@users.noreply.github.com>
2025-02-23 12:22:25 +0300

    chore: exclude test files from coverage (#25)

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vitest/config";
+import { configDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
@@ -7,7 +7,7 @@ export default defineConfig({
       provider: "v8",
       reporter: ["text", "json", "html"],
       include: ["**/src/**"],
-      exclude: ["**/examples/**"],
+      exclude: [...(configDefaults.coverage.exclude ?? []), "**/examples/**"],
     },
   },
 });
